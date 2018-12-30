3369b9f99af5ee0e3ad8e16217843219c6cc4d57
Shalev Abadi <shalev787@gmail.com>
2018-12-30 22:40:02 +0200

    extracted methods annouceTime

--- object oriented design/HW3/src/AnnounceTimeOnSeparateThread.java
+++ object oriented design/HW3/src/AnnounceTimeOnSeparateThread.java
@@ -1,31 +1,43 @@
 import java.applet.Applet;
 
-public class AnnounceTimeOnSeparateThread implements Runnable{
+public class AnnounceTimeOnSeparateThread implements Runnable {
 
 	int hour;
 	int minutes;
 	int sleepTime = 1000;
-	
+
 	public AnnounceTimeOnSeparateThread(int hour, int minutes) {
-		this. hour = hour;
-		this. minutes = minutes;
+		this.hour = hour;
+		this.minutes = minutes;
 	}
-	
-	
-	@SuppressWarnings("deprecation")
+
 	@Override
 	public void run() {
 		try {
-		Applet.newAudioClip(this.getClass().getResource("/audio/hour" + hour % 12 + ".au")).play();
-		Thread.sleep(sleepTime);
-		Applet.newAudioClip(this.getClass().getResource("/audio/minute" + minutes + ".au")).play();
-		Thread.sleep(sleepTime);
+			announceHour();
+			announceMinutes();
+			announceAmOrPm();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public void announceAmOrPm() throws InterruptedException {
 		String amOrPm = this.hour > 12 ? "pm" : "am";
 		Thread.sleep(sleepTime);
 		Applet.newAudioClip(this.getClass().getResource("/audio/" + amOrPm + ".au")).play();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	}
+
+	@SuppressWarnings("deprecation")
+	public void announceMinutes() throws InterruptedException {
+		Applet.newAudioClip(this.getClass().getResource("/audio/minute" + minutes + ".au")).play();
+		Thread.sleep(sleepTime);
+	}
+
+	@SuppressWarnings("deprecation")
+	public void announceHour() throws InterruptedException {
+		Applet.newAudioClip(this.getClass().getResource("/audio/hour" + hour % 12 + ".au")).play();
+		Thread.sleep(sleepTime);
 	}
 }
