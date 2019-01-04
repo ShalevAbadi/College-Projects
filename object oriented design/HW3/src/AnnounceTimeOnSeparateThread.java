83199b5a4e496cf496345efe70c16e7c8f53edea
Shalev Abadi <shalev787@gmail.com>
2019-01-04 09:12:17 +0200

    basically done, considering to add adapter interface

--- object oriented design/HW3/src/AnnounceTimeOnSeparateThread.java
+++ object oriented design/HW3/src/AnnounceTimeOnSeparateThread.java
@@ -1,43 +1,63 @@
-import java.applet.Applet;
+import java.applet.*;
 
+@SuppressWarnings("deprecation")
 public class AnnounceTimeOnSeparateThread implements Runnable {
 
 	int hour;
 	int minutes;
+	AudioClip[] hoursArr = new AudioClip[12];
+	AudioClip[] minutesArr = new AudioClip[60];
+	AudioClip am;
+	AudioClip pm;
+
 	int sleepTime = 1000;
 
 	public AnnounceTimeOnSeparateThread(int hour, int minutes) {
+		loadHoursToArr();
+		loadMinutesToArr();
+		loadAmPm();
+	}
+
+	private void loadAmPm() {
+		am = Applet.newAudioClip(this.getClass().getResource("/audio/am.au"));
+		pm = Applet.newAudioClip(this.getClass().getResource("/audio/pm.au"));
+	}
+
+	private void loadMinutesToArr() {
+		for (int i = 0; i < minutesArr.length; i++) {
+			minutesArr[i] = Applet.newAudioClip(this.getClass().getResource("/audio/minute" + i + ".au"));
+		}
+	}
+
+	private void loadHoursToArr() {
+		for (int i = 0; i < hoursArr.length; i++) {
+			hoursArr[i] = Applet.newAudioClip(this.getClass().getResource("/audio/hour" + i + ".au"));
+		}
+	}
+
+	public void setHour(int hour) {
 		this.hour = hour;
+	}
+
+	public void setMinutes(int minutes) {
 		this.minutes = minutes;
 	}
 
 	@Override
 	public void run() {
 		try {
-			announceHour();
-			announceMinutes();
+			hoursArr[hour].play();
+			Thread.sleep(sleepTime);
+			minutesArr[minutes].play();
+			Thread.sleep(sleepTime);
 			announceAmOrPm();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
-	@SuppressWarnings("deprecation")
-	public void announceAmOrPm() throws InterruptedException {
-		String amOrPm = this.hour > 12 ? "pm" : "am";
-		Thread.sleep(sleepTime);
-		Applet.newAudioClip(this.getClass().getResource("/audio/" + amOrPm + ".au")).play();
-	}
-
-	@SuppressWarnings("deprecation")
-	public void announceMinutes() throws InterruptedException {
-		Applet.newAudioClip(this.getClass().getResource("/audio/minute" + minutes + ".au")).play();
-		Thread.sleep(sleepTime);
-	}
-
-	@SuppressWarnings("deprecation")
-	public void announceHour() throws InterruptedException {
-		Applet.newAudioClip(this.getClass().getResource("/audio/hour" + hour % 12 + ".au")).play();
-		Thread.sleep(sleepTime);
+	public void announceAmOrPm() {
+		AudioClip amOrPm = hour > 12 ? am : pm;
+		amOrPm.play();
 	}
 }
